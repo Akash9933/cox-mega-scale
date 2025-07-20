363bfacfb8282f6a5ed231b1c92aa6a75a23de18
Akash9933 <38663700+Akash9933@users.noreply.github.com>
2025-07-20 15:52:12 -0700

    Commit finished plugin project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Example
-An example greeter plugin
+# CoxMegaScale
+Cox Mega Scale plugin helps with running a mega scale by providing utility and information.
+
+Provides:
+
+1. Scouting assistance by selecting whitelisted rooms and order.
+2. Points gained and lost.
+3. Drop chance with amount of rolls.
+4. How many overloads/golpar/fish needed.
--- src/test/java/com/coxmegascale/CoxMegaScalePluginTest.java
+++ src/test/java/com/coxmegascale/CoxMegaScalePluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.coxmegascale;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class CoxMegaScalePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CoxMegaScalePlugin.class);
 		RuneLite.main(args);
 	}
 }
